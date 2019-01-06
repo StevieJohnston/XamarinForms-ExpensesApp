d527411ba041ae9fae058d39c6cfab6e0f4a7aad
Manish Tiwari <manish.t92@gmail.com>
2019-01-06 16:35:20 +0530

    Custom Renderer on iOS

---
+++ XamarinForms-ExpensesApp/XamarinForms-ExpensesApp.iOS/CustomRenderers/CustomProgressbarRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinForms_ExpensesApp.iOS.CustomRenderers;
+using CoreGraphics;
+
+[assembly: ExportRenderer(typeof(ProgressBar), typeof(CustomProgressbarRenderer))]
+namespace XamarinForms_ExpensesApp.iOS.CustomRenderers
+{
+    public class CustomProgressbarRenderer : ProgressBarRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ProgressBar> e)
+        {
+            base.OnElementChanged(e);
+            LayoutSubviews();
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            float x = 1.0f;
+            float y = 4.0f;
+
+            CGAffineTransform transform = CGAffineTransform.MakeScale(x, y);
+            Transform = transform;
+        }
+    }
+}
